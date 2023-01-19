3a96f9bcdd8f24c068dfc864cfdc20f1e65d620c
Brock <brockburdine@gmail.com>
2023-01-19 10:27:59 -0600

    async/await

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const emailRef = document.querySelector(".email");
 
-// Then promise--- Use .then to unlock promises
-fetch("https://jsonplaceholder.typicode.com/users/1")
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-    emailRef.innerHTML = data.email
-  });
 
+//Async/Await
+async function main() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users/1");
+  const data = await response.json()
+  console.log(data);
+  emailRef.innerHTML = data.email
+}
+
+main();
